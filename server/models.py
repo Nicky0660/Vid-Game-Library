c93fe4a6255028a2a47a81d6e949a1823a13de02
Luis Westberry <takuame7@gmail.com>
2023-11-03 13:52:30 -0500

    filled out models with appropriate serialization and relationships

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.orm import validates
 from config import db
 
 # Models go here!
@@ -10,27 +10,65 @@ class Games(db.Model, SerializerMixin):
     __tablename__='games'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    release_yr = db.Column(db.Timestamp)
+    release_yr = db.Column(db.Integer)
     genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
+    serialize_rules = ('-console_games.game', '-genres.game')
 
+    genre = db.relationship('Genres', back_populates='game')
+    console_games = db.relationship('ConsoleGames', back_populates='game', cascade = 'all, delete-orphan')
+
+    @validates('title')
+    def validates_title(self, key, title):
+        if type (title) == str:
+            return title
+        else:
+            raise ValueError('Title must be a String')
+    @validates('release_yr')
+    def validates_release_yr(self, key, release_yr):
+        if type (release_yr) == int and len(str(release_yr)) == 4:
+            return release_yr
+        else:
+            raise ValueError('release_yr must be a Integer')
+   
+    def __repr__(self):
+        return f'<Games {self.id}: {self.title}>'
 
 class Genres(db.Model, SerializerMixin):
     __tablename__='genres'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    serialize_rules = ('-games.genre',)
 
+    games = db.relationship('Games', back_populates='genre')
 
 class ConsoleGames(db.Model, SerializerMixin):
     __tablename__='console_games'
     id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ('-console.console_games', '-game.console_games')
+
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
     console_id = db.Column(db.Integer, db.ForeignKey('consoles.id'))
+
+    game = db.relationship('Games', back_populates='console_games')
+    console = db.relationship('Consoles', back_populates='console_games')
 
 
 class Consoles(db.Model, SerializerMixin):
     __tablename__='consoles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    serialize_rules = ('-console_games.console', )
+
+    console_games = db.relationship('ConsoleGames', back_populates='console', cascade = 'all, delete-orphan')
+
+    @validates('name')
+    def validates_name(self, key, name):
+        if name : 
+            return name
+        else: 
+            raise ValueError('Console must have name')
+
+
 
 #optional stretch goal models
 class UserGames(db.Model, SerializerMixin):
