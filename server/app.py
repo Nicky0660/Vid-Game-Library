ff1f737401b1b5ec1daa66712155f364aafe483c
Luis Westberry <takuame7@gmail.com>
2023-11-05 15:20:08 -0600

    added inital routes for consoles, genres, and games. Set up the basic internals

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-
+from flask import Flask, make_response, jsonify, request
 # Remote library imports
 from flask import request
 from flask_restful import Resource
@@ -9,7 +9,7 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import db, Consoles, Games, ConsoleGames, Genres
 
 # Views go here!
 
@@ -17,6 +17,36 @@ from config import app, db, api
 def index():
     return '<h1>Project Server</h1>'
 
+#model routes below
+@app.route('/games', methods=['GET', 'POST'])
+def games():
+    if request.method == 'GET':
+        games = Games.query.all()
+        games_dict = [game.to_dict(rules=(''))for game in games]
+        response = make_response(games_dict, 200)
+    else:
+        response = make_response('Error games not found!', 404)
+    return response
+
+@app.route('/consoles', methods=['GET'])
+def consoles():
+    if request.method == 'GET':
+        consoles = Consoles.query.all()
+        consoles_dict = [console.to_dict(rules=(''))for console in consoles]
+        response = make_response(consoles_dict, 200)
+    else:
+        response = make_response('Error console not found!', 404)
+    return response
+
+@app.route('/genres', methods=['GET', 'POST', 'DELETE'])
+def genres():
+    if request.method == 'GET':
+        genres = Genres.query.all()
+        genres_dict = [genre.to_dict(rules=(''))for genre in genres]
+        response = make_response(genres_dict, 200)
+    else:
+        response = make_response('Error genres not found!', 404)
+    return response
 
 
 
