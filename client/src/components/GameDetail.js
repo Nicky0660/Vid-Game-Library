e54281f911509b78837aea56f4feaaf00a60e35d
Luis Westberry <takuame7@gmail.com>
2023-11-06 14:41:29 -0600

    double checking a previous push

--- client/src/components/GameDetail.js
+++ client/src/components/GameDetail.js
@@ -1,1 +1,2 @@
-//Displays detailed information about a single game, including title, genre, and platform.
+//Displays detailed information about a single game, including title, genre, and platform. 
+//Delete Button
