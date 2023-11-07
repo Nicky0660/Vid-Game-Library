7a55af4ea45cb9ed05378d0d5b3bb21ea2cc1782
shawnconway <shawnway210@gmail.com>
2023-11-07 13:56:05 -0500

    fetches

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,56 @@
 import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar"
 import { Switch, Route } from "react-router-dom";
+import Home from "./Home"
 
 // const baseURL = ('http://127.0.0.1:5555')
 
 function App() {
-  const [games, setGames] = useState([])
-  
+  const [gamesArray, setGamesArray] = useState([])
+  const[consoles, setConsoles]= useState([])
+  const[genres, setGenres]= useState([])
+
   useEffect(() => {
     fetch('http://127.0.0.1:5555/games')
       .then((res) => res.json())
-      .then((data) => setGames(data));
+      .then((data) => setGamesArray(data))
   },[])
-  console.log(games)
+  //console.log(data)
 
-  return <h1>Gaming Library</h1>;
+  useEffect(() => {
+    fetch('http://127.0.0.1:5555/consoles')
+      .then((res) => res.json())
+      .then((data) => setConsoles(data))
+  },[])
+  //console.log(data)
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5555/genres')
+      .then((res) => res.json())
+      .then((data) => setGenres(data))
+    },[])
+  
+  return (
+    <>
+    <header>
+      <h1>Game Library</h1>
+    </header>
+    <NavBar/>
+    <Switch>
+      <Route exact path="/">
+        <Home gamesArray={gamesArray}/>
+      </Route>
+      <Route exact path="/games">
+        
+      </Route>
+    </Switch>
+    </>
+ 
+  
+  
+  
+  
+  );
 }
 
 
